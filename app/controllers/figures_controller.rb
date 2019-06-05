5d6f544a4b2e8ed4500b1b0375abb416cdff0848
Jason Fournier <jmf4nier@hotmail.com>
2019-06-05 15:43:29 -0500

    in progress

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,18 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  set :views, './app/views/figures'
+  #index
+  get '/figures' do 
+    @figures = Figure.all
+    erb :index
+  end
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :new
+  end
+  post '/figures' do
+    # binding.pry
+    Figure.create(params)
+
+  end
 end
--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,12 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  set :views, './app/views/landmarks'
+  #index
+  get '/landmarks' do 
+    @landmarks = Landmark.all
+    erb :index
+  end
+  get '/landmarks/new' do
+    erb :new
+  end
 end
